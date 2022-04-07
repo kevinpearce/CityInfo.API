ddc4dad8b4dc0bcdc50a651955a6d99a67bae399
Kevin Pearce <Kevin.Pearce@ukho.gov.uk>
2022-04-07 15:16:35 +0100

    feat(db): initial db interaction - setup/create db - seed data via model builder context

--- CityInfo.API/DbContexts/CityInfoContext.cs
+++ CityInfo.API/DbContexts/CityInfoContext.cs
@@ -9,9 +9,68 @@ namespace CityInfo.API.DbContexts
         public DbSet<PointOfInterest> PointsOfInterest { get; set; } = null!;
 
         public CityInfoContext(DbContextOptions<CityInfoContext> options)
-            : base(options)
-        {
+            : base(options){}
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<City>()
+                .HasData(
+                new City("Bristol")
+                {
+                    Id = 1,
+                    Description = "The home of TripHop"
+                },
+                new City("Exeter")
+                {
+                    Id = 2,
+                    Description = "Memories of Flowerpot"
+                },
+                new City("Glastonbury")
+                {
+                    Id = 3,
+                    Description = "Never a dull moment"
+                });
+
+            modelBuilder.Entity<PointOfInterest>()
+                .HasData(
+                new PointOfInterest("The Hippodrome")
+                {
+                    Id = 1,
+                    CityId = 1,
+                    Description = "Theatre"
+                },
+                new PointOfInterest("St Georges Park")
+                {
+                    Id = 2,
+                    CityId = 1,
+                    Description = "Old School Snake Run"
+                },
+                new PointOfInterest("FlowerPot")
+                {
+                    Id = 3,
+                    CityId = 2,
+                    Description = "Modern Concrete"
+                },
+                new PointOfInterest("Ikea")
+                {
+                    Id = 4,
+                    CityId = 2,
+                    Description = "To lose the wife in"
+                },
+                new PointOfInterest("Town Centre")
+                {
+                    Id = 5,
+                    CityId = 3,
+                    Description = "Cafe Delight, maybe a Morris Dancer?"
+                },
+                new PointOfInterest("Somerton Road")
+                {
+                    Id = 6,
+                    CityId = 3,
+                    Description = "Long gone wooden park"
+                });
+
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
---
+++ CityInfo.API/Migrations/20220407141356_SeedData.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CityInfo.API.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Cities",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[] { 1, "The home of TripHop", "Bristol" });
+
+            migrationBuilder.InsertData(
+                table: "Cities",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[] { 2, "Memories of Flowerpot", "Exeter" });
+
+            migrationBuilder.InsertData(
+                table: "Cities",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[] { 3, "Never a dull moment", "Glastonbury" });
+
+            migrationBuilder.InsertData(
+                table: "PointsOfInterest",
+                columns: new[] { "Id", "CityId", "Description", "Name" },
+                values: new object[] { 1, 1, "Theatre", "The Hippodrome" });
+
+            migrationBuilder.InsertData(
+                table: "PointsOfInterest",
+                columns: new[] { "Id", "CityId", "Description", "Name" },
+                values: new object[] { 2, 1, "Old School Snake Run", "St Georges Park" });
+
+            migrationBuilder.InsertData(
+                table: "PointsOfInterest",
+                columns: new[] { "Id", "CityId", "Description", "Name" },
+                values: new object[] { 3, 2, "Modern Concrete", "FlowerPot" });
+
+            migrationBuilder.InsertData(
+                table: "PointsOfInterest",
+                columns: new[] { "Id", "CityId", "Description", "Name" },
+                values: new object[] { 4, 2, "To lose the wife in", "Ikea" });
+
+            migrationBuilder.InsertData(
+                table: "PointsOfInterest",
+                columns: new[] { "Id", "CityId", "Description", "Name" },
+                values: new object[] { 5, 3, "Cafe Delight, maybe a Morris Dancer?", "Town Centre" });
+
+            migrationBuilder.InsertData(
+                table: "PointsOfInterest",
+                columns: new[] { "Id", "CityId", "Description", "Name" },
+                values: new object[] { 6, 3, "Long gone wooden park", "Somerton Road" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "PointsOfInterest",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "PointsOfInterest",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "PointsOfInterest",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "PointsOfInterest",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "PointsOfInterest",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "PointsOfInterest",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
