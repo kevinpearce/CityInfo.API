c00ecdb3edfa10011410d279b51a13ea377a8c09
Kevin Pearce <Kevin.Pearce@ukho.gov.uk>
2022-04-08 09:50:01 +0100

    feat(*): implement more automapped db entities

--- CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -26,19 +26,21 @@ namespace CityInfo.API.Controllers
         }
 
         [HttpGet("{id}")]
-        public ActionResult<CityDto> GetCity(int id)
+        public async Task<IActionResult> GetCity(int id, bool includePointsOfInterest = false)
         {
-            //var cityToReturn = _citiesDataStore.Cities
-            //    .FirstOrDefault(c => c.Id == id);
+            var city = await _cityInfoRepository.GetCityAsync(id, includePointsOfInterest);
 
-            //if (cityToReturn == null)
-            //{
-            //    return NotFound();
-            //}
+            if (city == null)
+            {
+                return NotFound();
+            }
 
-            //return Ok(cityToReturn);
+            if (includePointsOfInterest)
+            {
+                return Ok(_mapper.Map<CityDto>(city));
+            }
 
-            return Ok(); //temp
+            return Ok(_mapper.Map<CityWithoutPointsOfInterestDto>(city));
         }
     }
 }
