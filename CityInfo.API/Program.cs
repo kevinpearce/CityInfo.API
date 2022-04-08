f595bd0c83874381ec5b5691e332a3bd701d56ae
Kevin Pearce <Kevin.Pearce@ukho.gov.uk>
2022-04-08 08:45:56 +0100

    feat(cityInfoRepository): add repository for db interaction

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -45,6 +45,8 @@ builder.Services.AddDbContext<CityInfoContext>(
     DbContextOptions => DbContextOptions.UseSqlite(
         builder.Configuration["ConnectionStrings:CityInfoDBConnectionString"]));
 
+builder.Services.AddScoped<ICityInfoRepository, CityInfoRepository>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
