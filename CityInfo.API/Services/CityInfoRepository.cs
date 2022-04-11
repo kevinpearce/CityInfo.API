b9e30caa44b0515d492a431f36bce6c107519204
Kevin Pearce <Kevin.Pearce@ukho.gov.uk>
2022-04-11 11:02:57 +0100

    feat(*): add filtering - city name filter added - include endpoint access

--- CityInfo.API/Services/CityInfoRepository.cs
+++ CityInfo.API/Services/CityInfoRepository.cs
@@ -19,6 +19,20 @@ namespace CityInfo.API.Services
                 .ToListAsync();
         }
 
+        public async Task<IEnumerable<City>> GetCitiesAsync(string? name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                return await GetCitiesAsync();
+            }
+
+            name = name.Trim();
+            return await _context.Cities
+                .Where(c => c.Name == name)
+                .OrderBy(c => c.Name)
+                .ToListAsync();
+        }
+
         public async Task<City?> GetCityAsync(int cityId, bool includePointsOfInterest)
         {
             if (includePointsOfInterest)
