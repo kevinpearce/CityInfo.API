13262052da5a1fbfbfbbdc3828c744e56c2fc535
Kevin Pearce <Kevin.Pearce@ukho.gov.uk>
2022-04-11 13:11:05 +0100

    feat(*): add jwt security token

---
+++ CityInfo.API/Controllers/AuthenticationController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/authentication")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        //only ever used here - hence scoped internally
+        public class AuthenticationRequestbody
+        {
+            public string? UserName { get; set; }
+            public string? Password { get; set; }
+        }
+
+        //only ever used here - hence scoped internally
+        private class CityInfoUser
+        {
+            public int UserId { get; set; }
+            public string UserName { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string City { get; set; }
+
+            public CityInfoUser(int userId, string userName, string firstName, string lastName, string city)
+            {
+                UserId = userId;
+                UserName = userName;
+                FirstName = firstName;
+                LastName = lastName;
+                City = city;
+            }
+        }
+
+        public AuthenticationController(IConfiguration configuration)
+        {
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        }
+
+        [HttpPost("authenticate")]
+        public ActionResult<string> Authenticate(AuthenticationRequestbody authenticationRequestbody)
+        {
+            //step1 - validate creds
+            var user = ValidateUserCredentials(authenticationRequestbody.UserName, authenticationRequestbody.Password);
+
+            if (user == null)
+            {
+                return Unauthorized();
+            }
+
+            //step2 - create a token
+            var securityKey = new SymmetricSecurityKey(
+                Encoding.ASCII.GetBytes(_configuration["Authentication:SecretForKey"]));
+
+            var signingCredentials = new SigningCredentials(
+                securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claimsForToken = new List<Claim>();
+            claimsForToken.Add(new Claim("sub", user.UserId.ToString()));
+            claimsForToken.Add(new Claim("given_name", user.FirstName));
+            claimsForToken.Add(new Claim("family_name", user.LastName));
+            claimsForToken.Add(new Claim("city", user.City));
+
+            var jwtSecurityToken = new JwtSecurityToken(
+                _configuration["Authentication:Issuer"],
+                _configuration["Authentication:Audience"],
+                claimsForToken,
+                DateTime.UtcNow, //token valid from (in amount of seconds passed since 01/01/1970)
+                DateTime.UtcNow.AddHours(1), //token expires on
+                signingCredentials);
+
+            var tokenToReturn = new JwtSecurityTokenHandler()
+                .WriteToken(jwtSecurityToken);
+
+            return Ok(tokenToReturn);
+        }
+
+        private CityInfoUser ValidateUserCredentials(string? userName, string? password)
+        {
+            //no db to check creds against
+            //FOR DEMO PURPOSES ONLY
+            return new CityInfoUser(1, userName ?? "", "Kevin", "Pearce", "Bristol");
+        }
+    }
+}
